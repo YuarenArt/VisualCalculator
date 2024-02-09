038ec3e65ace9b8e97f6f929759b2d12a15963fa
Yuriy Malyshev <100198677+YuarenArt@users.noreply.github.com>
2024-02-09 16:35:53 +0300

    Created new design, added StyleSheet.css

--- main.cpp
+++ main.cpp
@@ -1,34 +1,15 @@
 #include "./Calculators/visualcalculator.h"
-
-
-//class Singleton
-//{
-//public:
-//
-//    static Singleton& instance() {
-//
-//        static Singleton inst;
-//        return inst;
-//    }
-//    Singleton(const Singleton&) = delete;
-//    VisualCalculator& get() { return app_; }
-//
-//private:
-//
-//    Singleton() : app_() { }
-//    Singleton& operator= (const Singleton&) = delete;
-//
-//    VisualCalculator app_;
-//};
+#include <QStyleFactory>
+#include <QFile>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
 
-    /*Singleton& singleton = Singleton::instance();
-    VisualCalculator& calculator = singleton.get();
-    calculator.resize(350, 500);
-    calculator.show();*/
+    QFile styleFile("StyleSheet.css");
+    styleFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleFile.readAll());
+    qApp->setStyleSheet(styleSheet);
 
     VisualCalculator calculator;
     calculator.resize(350, 500);
